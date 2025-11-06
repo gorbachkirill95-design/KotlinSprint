367935a7d2057269da31ea2f25d2ee212460bdb4
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-06 13:28:16 +0300

    KS-1-3 work on mistakes

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -1,15 +1,13 @@
 package org.example.lesson1
 
-import kotlin.math.min
-
 fun main() {
     val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    var hour: String = "09"
+    var minute: String = "07"
     println("Год полета: $year")
     println("Час вылета: $hour")
     println("Минута вылета: $minute")
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     print("Посадка: $hour:$minute")
 }
