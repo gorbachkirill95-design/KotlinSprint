a0c51986c06da32d0aba5727b108ec8fb41501c4
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-06 13:39:06 +0300

    KS-1-4 spaces

--- src/main/kotlin/lesson1/lesson1_task4.kt
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -1,12 +1,14 @@
 package org.example.lesson1
 
 fun main() {
+
     val length: Long = 40_868_600_000L
     val age: Byte = 27
     val partOfDay: Float = 0.075F
     val seconds: Short  = 6480
     val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Short = 32700
+
     println(length)
     println(age)
     println(partOfDay)
