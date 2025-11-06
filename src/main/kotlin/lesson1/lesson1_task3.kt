f73b1182f856a5d88c48691ae1f4165c290de876
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-06 13:35:47 +0300

    KS-1-3 work on mistakes2

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -1,13 +1,17 @@
 package org.example.lesson1
 
 fun main() {
+
     val year: Int = 1961
     var hour: String = "09"
     var minute: String = "07"
+
     println("Год полета: $year")
     println("Час вылета: $hour")
     println("Минута вылета: $minute")
+
     hour = "10"
     minute = "55"
+
     print("Посадка: $hour:$minute")
 }
