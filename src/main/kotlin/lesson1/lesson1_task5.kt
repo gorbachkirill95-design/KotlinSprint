3b7a74f376752fc758b61f27b9e206386d773aba
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-07 14:01:35 +0300

    KS-1-5 work on mistakes

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,30 +1,15 @@
 package org.example.lesson1
 
+const val secondPerMinute = 60
+const val secondPerHour = 3600
+
 fun main() {
 
     val allSeconds: Int = 6480
-    val hours: Int = allSeconds / 3600
-    val minutes: Int = (allSeconds % 3600) / 60
-    val seconds: Int = allSeconds % 60
-    var hourString: String = hours.toString()
-    var minuteString: String = minutes.toString()
-    var secondString: String = seconds.toString()
+    val hours: Int = allSeconds / secondPerHour
+    val minutes: Int = (allSeconds % secondPerHour) / secondPerMinute
+    val seconds: Int = allSeconds % secondPerMinute
 
-    if (hourString.length == 1) {
-        hourString = "0$hours"
-    }
-    if (minuteString.length == 1) {
-        minuteString = "0$minuteString"
-    }
-    if (secondString.length == 1) {
-        secondString = "0$secondString"
-    }
-
-    println("Общее время в космосе: $hourString:$minuteString:$secondString")
-
-//    println("Общее время в космосе: %02d:%02d:%02d".format(hours, minutes, seconds))
-//    потом я нагуглил про метод .format,
-//    оставил оба варианта
-
+    println("Общее время в космосе: %02d:%02d:%02d".format(hours, minutes, seconds))
 
 }
