17db61f1f15182ad978c25215a645c696cb61a4b
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-07 08:49:16 +0300

    KS-1-4 work on mistakes

--- src/main/kotlin/lesson1/lesson1_task4.kt
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -5,9 +5,9 @@ fun main() {
     val length: Long = 40_868_600_000L
     val age: Byte = 27
     val partOfDay: Float = 0.075F
-    val seconds: Short  = 6480
+    val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
-    val apogee: Short = 32700
+    val apogee: Int = 327000
 
     println(length)
     println(age)
