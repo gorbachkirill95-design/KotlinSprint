29ce07efd97f4ade780c3a8cc1e4bdfcb54ab166
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-10 12:47:20 +0300

    KS-3-1

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson3
+
+fun main() {
+    val userName = "Кирилл"
+    var welcome = "Добрый день"
+
+    println("$welcome, $userName!")
+
+    welcome = "Добрый вечер"
+
+    println("$welcome, $userName!")
+}
