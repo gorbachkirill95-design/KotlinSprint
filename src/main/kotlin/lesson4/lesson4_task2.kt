daddf1a465bb7c3cd1a613a8028fe26cfba08dc7
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-10 14:46:46 +0300

    KS-4-2 mistakes

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -12,7 +12,7 @@ fun main() {
 
     println(
         "Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average': ${MIN_WEIGHT < weight1 && weight1 <= MAX_WEIGHT && volume1 < MAX_VOLUME}" +
-                "\nГруз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average': ${MIN_WEIGHT < weight2 && weight2 <= MAX_WEIGHT && 2 < MAX_VOLUME}"
+                "\nГруз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average': ${MIN_WEIGHT < weight2 && weight2 <= MAX_WEIGHT && volume2 < MAX_VOLUME}"
     )
 
 }
