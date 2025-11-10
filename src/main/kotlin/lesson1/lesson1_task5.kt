e09d264f554e26a2d6ae48aa7aab1b8457fddff0
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-10 12:14:27 +0300

    KS-1-5 refactor

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,14 +1,14 @@
 package org.example.lesson1
 
-const val secondPerMinute = 60
-const val secondPerHour = 3600
+const val SECOND_PER_MINUTE = 60
+const val SECOND_PER_HOUR = 3600
 
 fun main() {
 
     val allSeconds: Int = 6480
-    val hours: Int = allSeconds / secondPerHour
-    val minutes: Int = (allSeconds % secondPerHour) / secondPerMinute
-    val seconds: Int = allSeconds % secondPerMinute
+    val hours: Int = allSeconds / SECOND_PER_HOUR
+    val minutes: Int = (allSeconds % SECOND_PER_HOUR) / SECOND_PER_MINUTE
+    val seconds: Int = allSeconds % SECOND_PER_MINUTE
 
     println("Общее время в космосе: %02d:%02d:%02d".format(hours, minutes, seconds))
 
