308483a7fc6d210c2499cc80c7747725dfc95b90
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-10 12:29:54 +0300

    KS-2-4 refactor

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -3,10 +3,10 @@ package org.example.lesson2
 fun main() {
     val crystalOre: Int = 7
     val ferrumOre: Int = 11
-    val buffPercent: Double = 0.2
+    val buffPercent: Int = 20
 
-    val crystalBonus: Int = (crystalOre * buffPercent).toInt()
-    val ferrumBonus: Int = (ferrumOre * buffPercent).toInt()
+    val crystalBonus: Int = (crystalOre * buffPercent) / 100
+    val ferrumBonus: Int = (ferrumOre * buffPercent) / 100
 
     println("Кристалическая руда под баффом: $crystalBonus")
     println("Железная руда под баффом: $ferrumBonus")
