cb56535e075a2d6bb37e518aa4917d49f50fe568
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-10 12:20:28 +0300

    KS-2-3 refactor

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,17 +1,13 @@
 package org.example.lesson2
 
-const val minutePerHour = 60
+const val MINUTE_PER_HOUR = 60
 
 fun main() {
     val departureHour: Int = 9
     val departureMinute: Int = 39
     val travelTime: Int = 457
-    var arrivalHour: Int                 //могу ли я сразу здесь делать расчеты или это считается плохим тоном?
-    var arrivalMinute: Int
-    //или можно сделать отступ после travelTime и сразу проинициализировать их здесь и сразу же провести в них расчеты?
-    arrivalHour = ((departureHour * minutePerHour + departureMinute) + travelTime) / minutePerHour
-    arrivalMinute = ((departureHour * minutePerHour + departureMinute) + travelTime) % minutePerHour
-
+    var arrivalHour: Int = ((departureHour * MINUTE_PER_HOUR + departureMinute) + travelTime) / MINUTE_PER_HOUR
+    var arrivalMinute: Int = ((departureHour * MINUTE_PER_HOUR + departureMinute) + travelTime) % MINUTE_PER_HOUR
 
     println("Время прибытия поезда : %02d:%02d".format(arrivalHour, arrivalMinute))
 
