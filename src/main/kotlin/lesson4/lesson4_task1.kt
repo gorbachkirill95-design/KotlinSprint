9862d4a254cb0553e4aaa54922da01347289c0e2
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-10 14:23:35 +0300

    KS-4-1

---
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson4
+
+fun main() {
+    val tables: Int = 13
+    val today = 13
+    val tomorrow = 9
+    val reservToDay = tables > today
+    val reservTomorrow = tables > tomorrow
+
+    println("Доступность столиков на сегодня: $reservToDay\nДоступность столиков на завтра: $reservTomorrow")
+
+}
