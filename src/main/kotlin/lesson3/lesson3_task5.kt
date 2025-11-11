f623fa8212935df91e872ba9e98decf4ee1e0326
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-11 11:12:42 +0300

    KS-3-5 .split v2

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -2,12 +2,11 @@ package org.example.lesson3
 
 fun main() {
     val moveString = "D2-D4;0"
-    val parts: List<String> = moveString.split(";")
-    val moves: List<String> = parts[0].split("-")
-    val from = moves[0]
-    val to = moves[1]
-    val moveNumber = parts[1].toInt()
-//изначально я и делал через сплит, но потом подумал что коллекции мы еще не проходили и надо бы сделать по чарам строки :-D
+    val parts: List<String> = moveString.split(";", "-")
+    val from = parts[0]
+    val to = parts[1]
+    val moveNumber = parts[2]
+
     println("Откуда: $from")
     println("Куда: $to")
     println("Номер хода: $moveNumber")
