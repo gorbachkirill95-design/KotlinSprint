e12f276c0f8be9aadcf5b7fd64728d8867efa1ee
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-11 10:48:03 +0300

    KS-3-5 .split

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -1,12 +1,15 @@
 package org.example.lesson3
 
 fun main() {
-    val moveString: String = "D2-D4;0"
-    val from: String = moveString[0] + moveString[1].toString()
-    val to: String = moveString[3] + moveString[4].toString()
-    val numberMove = moveString[6].digitToInt()
-
+    val moveString = "D2-D4;0"
+    val parts: List<String> = moveString.split(";")
+    val moves: List<String> = parts[0].split("-")
+    val from = moves[0]
+    val to = moves[1]
+    val moveNumber = parts[1].toInt()
+//изначально я и делал через сплит, но потом подумал что коллекции мы еще не проходили и надо бы сделать по чарам строки :-D
     println("Откуда: $from")
     println("Куда: $to")
-    println("Номер хода: $numberMove")
+    println("Номер хода: $moveNumber")
+
 }
