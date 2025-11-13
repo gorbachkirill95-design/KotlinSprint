25b9e19838bc17d7ef15c00f521d4870527234f3
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-13 08:46:54 +0300

    KS-4-4 one val

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -2,16 +2,14 @@ package org.example.lesson4
 
 fun main() {
     val day: Int = 5
-    val armsDay: Boolean = day % 2 != 0
-    val legsDay: Boolean = day % 2 == 0
-    val pressDay: Boolean = armsDay
-    val rearDay: Boolean = legsDay
+    val evenDay: Boolean = day % 2 == 0
+
     println(
         """
-        Упражнения для рук:     $armsDay
-        Упражнения для ног:     $legsDay
-        Упражнения для спины:   $rearDay
-        Упражнения для пресса:  $pressDay
+        Упражнения для рук:     ${!evenDay}
+        Упражнения для ног:     $evenDay
+        Упражнения для спины:   $evenDay
+        Упражнения для пресса:  ${!evenDay}
     """.trimIndent()
     )
 }
