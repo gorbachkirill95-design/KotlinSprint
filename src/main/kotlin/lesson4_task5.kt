6eb2c7727847a4902f18ce027ed8ac7547c1ccbc
Kirillgorbach <gorbachkirill@gmail.com>
2025-11-13 08:31:05 +0300

    KS-4-5 const

--- src/main/kotlin/lesson4_task5.kt
+++ src/main/kotlin/lesson4_task5.kt
@@ -1,5 +1,9 @@
 package org.example
 
+const val MIN_COUNT_CREW = 55
+const val MAX_COUNT_CREW = 70
+const val MIN_COUNT_PROVISION = 50
+
 fun main() {
     print("Есть ли повреждения корпуса (true/false)? ")
     val damage = readLine()!!.toBoolean()
@@ -14,7 +18,8 @@ fun main() {
     val goodWeather = readLine()!!.toBoolean()
 
     val departure =
-        (!damage && crew in 55..70 && provisions > 50) || (damage && crew == 70 && goodWeather && provisions >= 50)
+        (!damage && crew in MIN_COUNT_CREW..MAX_COUNT_CREW && provisions > MIN_COUNT_PROVISION) ||
+                (damage && crew == MAX_COUNT_CREW && goodWeather && provisions >= MIN_COUNT_PROVISION)
 
     println("Корабль может отправиться в плавание: $departure")
 }
